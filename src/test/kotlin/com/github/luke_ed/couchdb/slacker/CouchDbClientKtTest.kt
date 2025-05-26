0b91b90a1bc4a2a893a47d05d86d71f7ed242c92
Luke Edmondson <luke_ed@outlook.com>
2025-05-26 18:42:42 -0500

    More setup of testing, still a lot remaining.

--- src/test/kotlin/com/github/luke_ed/couchdb/slacker/CouchDbClientKtTest.kt
+++ src/test/kotlin/com/github/luke_ed/couchdb/slacker/CouchDbClientKtTest.kt
@@ -2,6 +2,7 @@ package com.github.luke_ed.couchdb.slacker
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.github.luke_ed.couchdb.slacker.integration.TestDocument
 import okhttp3.Address
 import okhttp3.HttpUrl
 import okhttp3.HttpUrl.Companion.toHttpUrl
@@ -10,7 +11,11 @@ import okhttp3.OkHttpClient
 import okhttp3.Response
 import okhttp3.ResponseBody
 import okhttp3.ResponseBody.Companion.toResponseBody
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mock
@@ -22,29 +27,54 @@ import org.mockito.kotlin.whenever
 
 @ExtendWith(MockitoExtension::class)
 class CouchDbClientKtTest {
-  @Mock
-  private val httpClient: OkHttpClient = mock()
-  @Mock
-  private val address: Address = mock()
-  @Mock
-  private val dbContext: CouchDbContext = mock()
+  @Mock private val httpClient: OkHttpClient = mock()
+  @Mock private val address: Address = mock()
+  @Mock private val dbContext: CouchDbContext = mock()
 
   private val baseUrl: HttpUrl = "http://localhost:5984".toHttpUrl()
   private lateinit var couchDbClientKt: CouchDbClientKt
 
   @BeforeEach
   fun beforeEach() {
-    couchDbClientKt = CouchDbClientKt(httpClient, address, listOf(), 8, 3, false, 10000,
-      QueryStrategy.MANGO, ObjectMapper().registerModule(KotlinModule.Builder().build()), dbContext,baseUrl)
+    couchDbClientKt =
+      CouchDbClientKt(
+        httpClient,
+        address,
+        listOf(),
+        8,
+        3,
+        false,
+        10000,
+        QueryStrategy.MANGO,
+        ObjectMapper().registerModule(KotlinModule.Builder().build()),
+        dbContext,
+        baseUrl
+      )
   }
 
   @Test
+  fun testGetDataBaseName() {
+    whenever(dbContext[TestDocument::class.java])
+      .doReturn(EntityMetadata(DocumentDescriptor.of(TestDocument::class.java)))
+
+    val databaseName: String = couchDbClientKt.getDataBaseName(TestDocument::class.java)
+    assertThat(databaseName, equalTo("test"))
+  }
+
+  @Disabled("Not fully implemented, need to figure out the mocking")
+  @Test
   fun testSaveNewItem() {
-    val mResponseBody: ResponseBody = """{"id": "unique", "rev": "revision", "ok": "true"}""".toResponseBody(
-      "application/json".toMediaType()
-    )
+    val mResponseBody: ResponseBody =
+      """{"id": "unique", "rev": "revision", "ok": "true"}"""
+        .toResponseBody("application/json".toMediaType())
     val mResponse: Response = mock { on { body } doReturn mResponseBody }
 
-    whenever( httpClient.newCall(any()).execute() ).thenReturn( mResponse)
+    whenever(dbContext[TestDocument::class.java])
+      .doReturn(EntityMetadata(DocumentDescriptor.of(TestDocument::class.java)))
+
+    whenever(httpClient.newCall(any()).execute()).thenReturn(mResponse)
+
+    val savedDocument = couchDbClientKt.save(TestDocument("test"))
+    assertNotNull(savedDocument)
   }
 }
