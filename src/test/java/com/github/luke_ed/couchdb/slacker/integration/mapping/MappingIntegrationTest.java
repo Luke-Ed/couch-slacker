964ab0398b02eb3305e050e3c74cec38b6d517f9
Luke Edmondson <luke_ed@outlook.com>
2025-05-26 09:39:13 -0500

    Migrated MappingIntegrationTest and ViewedDocumentsIntegrationTest to using test containers.

--- src/test/java/com/github/luke_ed/couchdb/slacker/integration/mapping/MappingIntegrationTest.java
+++ src/test/java/com/github/luke_ed/couchdb/slacker/integration/mapping/MappingIntegrationTest.java
@@ -21,6 +21,7 @@ import com.github.luke_ed.couchdb.slacker.CouchDbContext;
 import com.github.luke_ed.couchdb.slacker.annotation.EnableCouchDbRepositories;
 import com.github.luke_ed.couchdb.slacker.integration.TestDocument;
 import com.github.luke_ed.couchdb.slacker.integration.TestDocumentRepository;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -33,9 +34,16 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.utility.DockerImageName;
 
 import java.io.IOException;
+import java.util.Map;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -59,6 +67,37 @@ class MappingIntegrationTest {
 
     @Autowired
     TestDocumentRepository repository;
+
+    @Container
+    private static final GenericContainer<?> singleCouchDbContainer;
+
+    static {
+        singleCouchDbContainer = new GenericContainer<>(DockerImageName.parse("couchdb:latest"))
+                .withEnv(Map.of(
+                        "COUCHDB_USER", "admin",
+                        "COUCHDB_PASSWORD", "-pbkdf2-2a4bb055c2a66b28158523b86afcd7f63bc67ef1,WnMIf5ph/d+jVJDA4WjXdw==,10",
+                        "COUCHDB_SECRET", "0123456789abcdef0123456789abcdef"
+                ))
+                .withExposedPorts(5984, 5986)
+                .waitingFor(Wait.forListeningPort().forPorts(5984, 5986))
+                .waitingFor(Wait.forHttp("/_up").forPort(5984).forStatusCode(200));
+
+        singleCouchDbContainer.start();
+    }
+
+    @DynamicPropertySource
+    static void viewStrategyProperties(DynamicPropertyRegistry registry) {
+        registry.add("couchdb.client.url", () -> "http://%s:%d"
+                .formatted(singleCouchDbContainer.getHost(), singleCouchDbContainer.getFirstMappedPort())
+        );
+    }
+
+    @AfterAll
+    void afterAll() {
+        if (singleCouchDbContainer.isRunning()) {
+            singleCouchDbContainer.stop();
+        }
+    }
 
     @BeforeEach
     void clear() {
