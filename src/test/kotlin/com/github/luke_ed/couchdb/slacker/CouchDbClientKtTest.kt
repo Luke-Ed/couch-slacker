3f07264f6bcf63e8f8318da975993c4be9cc5e18
Luke Edmondson <luke_ed@outlook.com>
2025-05-26 14:09:28 -0500

    Started framework of kotlin CouchDbClient test

---
+++ src/test/kotlin/com/github/luke_ed/couchdb/slacker/CouchDbClientKtTest.kt
@@ -0,0 +1,50 @@
+package com.github.luke_ed.couchdb.slacker
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import okhttp3.Address
+import okhttp3.HttpUrl
+import okhttp3.HttpUrl.Companion.toHttpUrl
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import okhttp3.ResponseBody
+import okhttp3.ResponseBody.Companion.toResponseBody
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.any
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+
+@ExtendWith(MockitoExtension::class)
+class CouchDbClientKtTest {
+  @Mock
+  private val httpClient: OkHttpClient = mock()
+  @Mock
+  private val address: Address = mock()
+  @Mock
+  private val dbContext: CouchDbContext = mock()
+
+  private val baseUrl: HttpUrl = "http://localhost:5984".toHttpUrl()
+  private lateinit var couchDbClientKt: CouchDbClientKt
+
+  @BeforeEach
+  fun beforeEach() {
+    couchDbClientKt = CouchDbClientKt(httpClient, address, listOf(), 8, 3, false, 10000,
+      QueryStrategy.MANGO, ObjectMapper().registerModule(KotlinModule.Builder().build()), dbContext,baseUrl)
+  }
+
+  @Test
+  fun testSaveNewItem() {
+    val mResponseBody: ResponseBody = """{"id": "unique", "rev": "revision", "ok": "true"}""".toResponseBody(
+      "application/json".toMediaType()
+    )
+    val mResponse: Response = mock { on { body } doReturn mResponseBody }
+
+    whenever( httpClient.newCall(any()).execute() ).thenReturn( mResponse)
+  }
+}
