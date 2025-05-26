5fe1db88b9d34c8c8cbf320b6203c81f4f6370ee
Luke Edmondson <luke_ed@outlook.com>
2025-05-26 12:19:41 -0500

    Updated to test non-depreciated method.

--- src/test/java/com/github/luke_ed/couchdb/slacker/repository/CouchDbRepositoryConfigurationExtensionTest.java
+++ src/test/java/com/github/luke_ed/couchdb/slacker/repository/CouchDbRepositoryConfigurationExtensionTest.java
@@ -25,7 +25,7 @@ class CouchDbRepositoryConfigurationExtensionTest {
     @Test
     void test() {
         CouchDbRepositoryConfigurationExtension extension = new CouchDbRepositoryConfigurationExtension();
-        assertEquals("couchDb", extension.getModulePrefix(), "Extension must return couchDb as prefix of named queries lookup");
+        assertEquals("couchDb", extension.getModuleName(), "Extension must return couchDb as prefix of named queries lookup");
         assertEquals(CouchDbRepositoryFactoryBean.class.getName(), extension.getRepositoryFactoryBeanClassName(),
                 "Extension must return " + CouchDbRepositoryFactoryBean.class + "as the extension");
     }
